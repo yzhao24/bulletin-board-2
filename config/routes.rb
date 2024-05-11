fd382475fa5918c7adb49557be9c3c9a5053b325
yzhao24 <165528874+yzhao24@users.noreply.github.com>
2024-05-11 22:16:36 +0000

    device installed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get("/", { :controller => "boards", :action => "index" })
+  root to: "boards#index"
+  #get("/", { :controller => "boards", :action => "index" })
 
   # Routes for the Post resource:
 
